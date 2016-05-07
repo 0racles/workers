621e71142b53ca1c930cf2d6777aee525e280a57
0racles <0racles@users.noreply.github.com>
2016-05-07 18:29:53 +0200

    making further corrections

--- sw.js
+++ sw.js
@@ -1,6 +1,1 @@
-if ('serviceWorker' in navigator) { 
-navigator.serviceWorker.register('/workers/sw.js', {scope : '/workers/'}).then(function(reg) { 
-	console.log('succesfully registered.scope is ' + reg.scope); }).catch(function(error) { 
-		console.log('not succesfully registered. failed with ' + error); 
-	});
-}; 
+
